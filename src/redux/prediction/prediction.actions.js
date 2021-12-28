aa18f7eb74f8107cf26ca64f1267fd87b9fbaf66
dalabdulaali <dalabdulaali>
2021-12-28 03:50:31 +0300

    URLL

--- src/redux/prediction/prediction.actions.js
+++ src/redux/prediction/prediction.actions.js
@@ -48,7 +48,7 @@ export const predict = (imgBase64) =>{
                     }
                 }
             ],
-            endpoint: '/testing',
+            endpoint: 'https://84qndld5j6.execute-api.us-east-2.amazonaws.com/testing',
             method: 'POST',
             headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": '*' },
             body: imgBase64,
